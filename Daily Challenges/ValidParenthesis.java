b91e949ead21a23f550766605b2fb153be2ae440
Aritra Das <aritrad917@gmail.com>
2022-03-13 14:33:42 +0530

    Valid Parenthesis added

---
+++ Daily Challenges/ValidParenthesis.java
@@ -0,0 +1,54 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+ 
+Example 1:
+
+Input: s = "()"
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+Output: false
+
+
+METHOD:(STACK)
+
+TIME: O(len(S)).
+*/
+
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> st = new Stack<>();
+        
+        for(char c : s.toCharArray()){
+            
+            if(c == '('){
+                st.push(')');
+            }
+            else if(c == '{'){
+                st.push('}');
+            }
+            else if(c == '['){
+                st.push(']');
+            }
+            else{
+                if(st.isEmpty() || st.pop() != c){
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
