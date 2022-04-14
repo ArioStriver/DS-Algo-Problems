dab4547787d25e54b20e94d39c50f63495430216
Aritra Das <aritrad917@gmail.com>
2022-04-15 02:16:33 +0530

    Search in a Binary Search Tree added

---
+++ Daily Challenges/SearchinaBST.java
@@ -0,0 +1,41 @@
+/*
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+Example 1:
+
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+
+Example 2:
+
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+
+
+METHOD:
+
+TIME: O(logN).
+
+SPACE: O(logN).
+*/
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        
+        if(root == null){
+            return null;
+        }
+        
+        if(root.val == val){
+            return root;
+        }
+        
+        if(root.val > val){
+           return searchBST(root.left, val);
+        }
+        
+        return searchBST(root.right, val);
+    }
+}
